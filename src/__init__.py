47ae080ebff4a547169e144ac53c5acb046a8ded
Alihcyv <elihcyv@gmail.com>
2026-04-29 20:20:56 +0400

    Initialize src module with imports

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.config import CFG
+from src.dataset import ImageDataset
+from src.model import load_model
+from src.trainer import train_one_fold, run_epoch, evaluate_model
+from src.inference import run_inference
